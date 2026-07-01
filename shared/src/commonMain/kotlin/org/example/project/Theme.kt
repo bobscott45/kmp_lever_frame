6268a320252c9aa02a6fcfe1d4d88f2bc9aae547
robert <bob@rwscott.co.uk>
2026-07-01 17:35:33 +0100

    Tweak Distant Signal color to be more yellow

--- shared/src/commonMain/kotlin/org/example/project/Theme.kt
+++ shared/src/commonMain/kotlin/org/example/project/Theme.kt
@@ -9,7 +9,7 @@ object LeverFrameTheme {
         
         // Lever Types
         val HomeSignal = Color(0xFF8f2727)
-        val DistantSignal = Color(0xFFb08817)
+        val DistantSignal = Color(0xFFa5a51c)
         val Points = Color(0xFF000000)
         val FacingPoints = Color(0xFF2b58b5)
         val Brown = Color(0xFF5C4033)
